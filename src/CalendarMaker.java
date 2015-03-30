4d15bda1ed10251f2e77a65802e2f97a0bb6cd30
Jason Tanabe <jasonmt@hawaii.edu>
2015-03-29 22:51:48 -1000

    Almost working Part2 and class modifications
    -Changed DateData class to use Calendar class instead
    -Created DateBlockData class for start/end dates
    -Can now compare Dates (use to sort)
    -Can take in and parse input files for dates

--- src/CalendarMaker.java
+++ src/CalendarMaker.java
@@ -1,89 +1,133 @@
+import java.io.File;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class CalendarMaker {
-	private final static int PUBLIC_PRIVATE = 1;
-	private final static int PRIORITY = 2;
-	private final static int LOCATION = 3;
-	private final static int SUMMARY = 4;
-	private final static int START_TIME = 5;
-	private final static int END_TIME = 6;
-	private final static int SHOW_DATA = 7;
-	private final static int MAKE_CALENDAR = 8;
-	private final static int QUIT = 9;
+	private static final DateFormat ICS_FORMAT_DATE = new SimpleDateFormat("yyyyMMdd");
+	private static final DateFormat ICS_FORMAT_TIME = new SimpleDateFormat("HHmmss");
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy");
+	private static final DateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+	private final static int 		PUBLIC_PRIVATE = 1;
+	private final static int 		PRIORITY = 2;
+	private final static int 		LOCATION = 3;
+	private final static int 		SUMMARY = 4;
+	private final static int 		START_TIME = 5;
+	private final static int 		END_TIME = 6;
+	private final static int		CREATE_FREE_TIME = 7;
+	private final static int 		SHOW_DATA = 8;
+	private final static int 		MAKE_CALENDAR = 9;
+	private final static int 		QUIT = 10;
 	
 	private boolean 				isPublic;
 	private int 					priority;
 	private String					location;
 	private String					summary;
 
-	private class DateData {
-		public int month;
-		public int day;
-		public int year;
-		public int hour;
-		public int minute;
-		public int second;
+	private class DateData implements Comparable {
+		public Calendar cal;
 
 		public DateData() {
-			month = 0;
-			day = 0;
-			year= 0;
-			hour = 0;
-			minute = 0;
-			second = 0;
+			cal = Calendar.getInstance();
+		}
+		
+		public DateData(String data) {
+			int year = Integer.parseInt(data.substring(0, 4));
+			int month = Integer.parseInt(data.substring(4, 6));
+			int day = Integer.parseInt(data.substring(6, 8));
+			int hour = Integer.parseInt(data.substring(9, 11));
+			int minute = Integer.parseInt(data.substring(11, 13));
+			int second = Integer.parseInt(data.substring(13, 15));
+			// possibly could check if data is valid over here
+			cal = Calendar.getInstance();
+			cal.set(year, month-1, day, hour, minute, second);
+		}
+		
+		public boolean isSameDate(DateData other) {
+			return cal.get(Calendar.MONTH) == other.cal.get(Calendar.MONTH) && 
+					other.cal.get(Calendar.YEAR) == cal.get(Calendar.YEAR) && 
+					other.cal.get(Calendar.DAY_OF_MONTH) == cal.get(Calendar.DAY_OF_MONTH);
 		}
 		
 		public String format() {
-			String format = new String(String.format("%04d", year) +
-					String.format("%02d", month) + String.format("%02d", day) +
-					"T" + String.format("%02d", hour) + String.format("%02d", minute) +
-					String.format("%02d", second));
-			return format;
+			return ICS_FORMAT_DATE.format(cal.getTime()) + "T" + ICS_FORMAT_TIME.format(cal.getTime());
 		}
 
 		public void show() {
-			System.out.println("Month/Day/Year: " + month + "/" + day + "/" + year);
-			System.out.println("Hour:Minute:Second: " + hour + ":" + minute + ":" + second);
+			System.out.println("Month/Day/Year: " + DATE_FORMAT.format(cal.getTime()));
+			System.out.println("Hour:Minute:Second: " + TIME_FORMAT.format(cal.getTime()));
 		}
 		
 		public boolean grab() {
 			System.out.println("Enter Month (1-12):");
 			int inputInt = Integer.parseInt(scanner.nextLine());
 			if (inputInt < 1 || inputInt > 12) return false;
-			month = inputInt;
+			cal.set(Calendar.MONTH, inputInt-1);
 
-			System.out.println("Enter Day (1-32):");
+			System.out.println("Enter Day (1-31):");
 			inputInt = Integer.parseInt(scanner.nextLine());
-			if (inputInt < 1 || inputInt > 32) return false;
-			day = inputInt;
+			if (inputInt < 1 || inputInt > 31) return false;
+			cal.set(Calendar.DAY_OF_MONTH, inputInt);
 
 			System.out.println("Enter Year (0-9999):");
 			inputInt = Integer.parseInt(scanner.nextLine());
 			if (inputInt < 0 || inputInt > 9999) return false;
-			year = inputInt;
+			cal.set(Calendar.YEAR, inputInt);
 
 			System.out.println("Enter Hour (24-hr format):");
 			inputInt = Integer.parseInt(scanner.nextLine());
 			if (inputInt < 0 || inputInt > 23) return false;
-			hour = inputInt;
+			cal.set(Calendar.HOUR_OF_DAY, inputInt);
 			
 			System.out.println("Enter Minute (0-59):");
 			inputInt = Integer.parseInt(scanner.nextLine());
 			if (inputInt < 0 || inputInt > 59) return false;
-			minute = inputInt;
+			cal.set(Calendar.MINUTE, inputInt);
 
 			System.out.println("Enter Second (0-59):");
 			inputInt = Integer.parseInt(scanner.nextLine());
 			if (inputInt < 0 || inputInt > 59) return false;
-			second = inputInt;
+			cal.set(Calendar.SECOND, inputInt);
 
 			return true;
 		}
+
+		@Override
+		public int compareTo(Object arg0) {
+			DateData other = (DateData) arg0;
+			if (cal.getTime().before(other.cal.getTime())) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 	}
-	private DateData				startTime;
-	private DateData				endTime;
+	private class DateBlockData implements Comparable {
+		public DateData			startTime;
+		public DateData			endTime;
+		
+		public DateBlockData() {
+			startTime = new DateData();
+			endTime = new DateData();
+		}
+		
+		public DateBlockData(String start, String end) {
+			startTime = new DateData(start);
+			endTime = new DateData(end);
+		}
+
+		@Override
+		public int compareTo(Object arg0) {
+			DateBlockData other = (DateBlockData) arg0;
+			return startTime.compareTo(other.startTime);
+		}
+	}
 	
+	private DateBlockData 			dateBlock;	
 	private Scanner					scanner;
 	
 	public static void main(String[] args) {
@@ -97,8 +141,7 @@ public class CalendarMaker {
 		priority = 1;
 		location =  new String("");
 		summary = new String("");
-		startTime = new DateData();
-		endTime = new DateData();
+		dateBlock = new DateBlockData();
 	}
 	
 	public void start() {
@@ -106,7 +149,8 @@ public class CalendarMaker {
 		boolean test = true;
 		while (inputInt != QUIT) {
 			System.out.println("(1)Public/Private, (2)Priority, (3)Location, " +
-					"(4)Summary, (5)Start, (6)End, (7)Show Data, (8)Make Calendar, (9)Quit");
+					"(4)Summary, (5)Start, (6)End, (7)Create Free Time, (8)Show Data, " +
+					"(9)Make Calendar, (10)Quit");
 			try {
 				inputInt = Integer.parseInt(scanner.nextLine());
 			} catch (Exception e) {
@@ -132,13 +176,17 @@ public class CalendarMaker {
 			case END_TIME:
 				test = grabEnd();
 				break;
+			case CREATE_FREE_TIME:
+				System.out.println("Enter filenames separated by ',' (test.txt,test2.txt): ");
+				createFreeTime(scanner.nextLine().split(","));
+				break;
 			case SHOW_DATA:
 				showCurrentData();
 				break;
 			case MAKE_CALENDAR:
 				System.out.println("Enter a filename to save data");
-				String fileName = scanner.nextLine();
-				makeCalendar(fileName);
+				String filename = scanner.nextLine();
+				makeCalendar(filename);
 				break;
 			case QUIT:
 				System.out.println("Quitting Program");
@@ -150,6 +198,33 @@ public class CalendarMaker {
 				System.out.println("Invalid Data Entered");
 			}
 		}
+	}
+	
+	public void createFreeTime(String[] filenames) {
+		ArrayList<DateBlockData> dateBlocks = new ArrayList<DateBlockData>();
+		for (String filename : filenames) {
+			if (validFile(filename)) {
+				File file = new File(filename);
+				Scanner fileScanner = null;
+				try {
+				fileScanner = new Scanner(file);
+				} catch (Exception e) {}
+				String start = null;
+				String end = null;
+				while (fileScanner.hasNext()) {
+					String[] splitLine = fileScanner.nextLine().split(":");
+					if (stringMatch(splitLine[0], "DTSTART")) start = splitLine[1];
+					if (stringMatch(splitLine[0], "DTEND")) end = splitLine[1];
+				}
+				if (start != null && end != null) {
+					DateBlockData block = new DateBlockData(start, end);
+					block.startTime.show();
+					block.endTime.show();
+					dateBlocks.add(block);
+				}
+			}
+		}
+		System.out.println(Integer.toString(dateBlocks.get(1).compareTo(dateBlocks.get(0))));
 	}
 	
 	public boolean grabPublic() {
@@ -184,12 +259,12 @@ public class CalendarMaker {
 	
 	public boolean grabStart() {
 		System.out.println("------StartTime------");
-		return startTime.grab();
+		return dateBlock.startTime.grab();
 	}
 	
 	public boolean grabEnd() {
 		System.out.println("-------EndTime-------");
-		return endTime.grab();
+		return dateBlock.endTime.grab();
 	}
 	
 	public void showCurrentData() {
@@ -221,21 +296,20 @@ public class CalendarMaker {
 	
 	public void showStartTime() {
 		System.out.println("------StartTime------");
-		startTime.show();
+		dateBlock.startTime.show();
 		System.out.println("---------------------");
 	}
 	
 	public void showEndTime() {
 		System.out.println("-------EndTime-------");
-		endTime.show();
+		dateBlock.endTime.show();
 		System.out.println("---------------------");
 	}
 	
-	public void makeCalendar(String fileName) {
-		String inputString = null;
+	public void makeCalendar(String filename) {
 		PrintWriter writer = null;
 		try {
-			writer = new PrintWriter(fileName, "UTF-8");
+			writer = new PrintWriter(filename, "UTF-8");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Error with file name");
@@ -252,12 +326,26 @@ public class CalendarMaker {
 		writer.println("PRIORITY:" + priority);
 		writer.println("LOCATION:" + location);
 		writer.println("SUMMARY:" + summary);
-		writer.println("DTSTART;TZID=Pacific/Honolulu:" + startTime.format());
-		writer.println("DTEND;TZID=Pacific/Honolulu:" + endTime.format());
+		writer.println("DTSTART;TZID=Pacific/Honolulu:" + dateBlock.startTime.format());
+		writer.println("DTEND;TZID=Pacific/Honolulu:" + dateBlock.endTime.format());
 		writer.println("END:VEVENT");
 		writer.println("END:VCALENDAR");
 		System.out.println("Finished Creating File");
 		writer.close();	
+	}
+	
+	private boolean validFile(String filename) {
+		File file = new File(filename);
+		if (file.exists() && !file.isDirectory()) return true;
+		return false;
+	}
+	
+	private boolean stringMatch(String string, String pattern) {
+		if (string.isEmpty() || pattern.isEmpty() || pattern.length() > string.length()) return false;
+		for (int i = 0; i < pattern.length(); i++) {
+			if (string.charAt(i) != pattern.charAt(i)) return false;
+		}
+		return true;
 	}
 	
 }
