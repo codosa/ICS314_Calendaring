1d41574d78d8cca6f79bb274d2e2ee907d321108
Jason Tanabe <jasonmt@hawaii.edu>
2015-04-17 18:57:40 -1000

    Finished Checkin 3
    -Split free times function into multiple smaller ones
    -Created new function to create meetings times
    -Meeting times are the same as free time, but two lists

--- src/CalendarMaker.java
+++ src/CalendarMaker.java
@@ -13,24 +13,25 @@ import java.util.Scanner;
 // more tests for createFreeTime 
 
 public class CalendarMaker {
-  private final static int          PUBLIC_PRIVATE = 1;
-  private final static int          PRIORITY = 2;
-  private final static int          LOCATION = 3;
-  private final static int          SUMMARY = 4;
-  private final static int          START_TIME = 5;
-  private final static int          END_TIME = 6;
-  private final static int          CREATE_FREE_TIME = 7;
-  private final static int          SHOW_DATA = 8;
-  private final static int          MAKE_CALENDAR = 9;
-  private final static int          QUIT = 10;
+  private final static int PUBLIC_PRIVATE = 1;
+  private final static int PRIORITY = 2;
+  private final static int LOCATION = 3;
+  private final static int SUMMARY = 4;
+  private final static int START_TIME = 5;
+  private final static int END_TIME = 6;
+  private final static int CREATE_FREE_TIME = 7;
+  private final static int CREATE_MEETING_TIME = 8;
+  private final static int SHOW_DATA = 9;
+  private final static int MAKE_CALENDAR = 10;
+  private final static int QUIT = 11;
 
-  private boolean                   mIsPublic;
-  private int                       mPriority;
-  private String                    mLocation;
-  private String                    mSummary;
+  private boolean mIsPublic;
+  private int mPriority;
+  private String mLocation;
+  private String mSummary;
 
-  private DateBlockData             mDateBlock;
-  private Scanner                   mScanner;
+  private DateBlockData mDateBlock;
+  private Scanner mScanner;
 
   public static void main(String[] args) {
     CalendarMaker maker = new CalendarMaker();
@@ -51,8 +52,8 @@ public class CalendarMaker {
     boolean test = true;
     while (inputInt != QUIT) {
       System.out.println("(1)Public/Private, (2)Priority, (3)Location, "
-          + "(4)Summary, (5)Start, (6)End, (7)Create Free Time, (8)Show Data, "
-          + "(9)Make Calendar, (10)Quit");
+          + "(4)Summary, (5)Start, (6)End, (7)Create Free Time, (8)Create Meeting Times, " +
+          "(9)Show Data, (10)Make Calendar, (11)Quit");
       try {
         inputInt = Integer.parseInt(mScanner.nextLine());
       } catch (Exception e) {
@@ -81,7 +82,16 @@ public class CalendarMaker {
       case CREATE_FREE_TIME:
         System.out
             .println("Enter filenames separated by ',' (test.txt,test2.txt): ");
-        createFreeTime(mScanner.nextLine().split(","));
+        createFreeTimes(mScanner.nextLine().split(","));
+        break;
+      case CREATE_MEETING_TIME:
+        System.out
+            .println("Enter first list of files separated by ',' (test.txt,test2.txt): ");
+        String[] list1 = mScanner.nextLine().split(",");
+        System.out
+            .println("Enter second list of files separated by ',' (test.txt,test2.txt): ");
+        String[] list2 = mScanner.nextLine().split(",");
+        createMeetingTimes(list1, list2);
         break;
       case SHOW_DATA:
         showCurrentData();
@@ -103,62 +113,36 @@ public class CalendarMaker {
     }
   }
 
-  public void createFreeTime(String[] filenames) {
-    ArrayList<DateBlockData> mDateBlocks = new ArrayList<DateBlockData>();
-    for (String filename : filenames) {
-      if (validFile(filename)) {
-        File file = new File(filename);
-        Scanner fileScanner = null;
-        try {
-          fileScanner = new Scanner(file);
-        } catch (Exception e) {
-        }
-        String start = null;
-        String end = null;
-        // parse .ics for DTSTART and DTEND and init objects
-        while (fileScanner.hasNext()) {
-          String[] splitLine = fileScanner.nextLine().split(":");
-          if (stringMatch(splitLine[0], "DTSTART"))
-            start = splitLine[1];
-          if (stringMatch(splitLine[0], "DTEND"))
-            end = splitLine[1];
-        }
-        // if the .ics file has proper DTSTART and DTEND
-        if (start != null && end != null) {
-          DateBlockData block = new DateBlockData(start, end);
-          block.getStartTime().show();
-          block.getEndTime().show();
-          // all date blocks must be on the same day, only add if they are
-          if (mDateBlocks.isEmpty() || (!mDateBlocks.isEmpty() && 
-              DateData.isSameDate(mDateBlocks.get(0).getStartTime(), block.getStartTime()))) {
-            mDateBlocks.add(block);
-          }
-        }
-      }
-    }
-    if (mDateBlocks.size() == 0) {
-      return;
-    }
-    int year = mDateBlocks.get(0).getStartTime().getCal().get(Calendar.YEAR);
-    // add one because cal.get(Calendar.MONTH) starts at 0 for jan
-    int month = mDateBlocks.get(0).getStartTime().getCal().get(Calendar.MONTH) + 1;
-    int day = mDateBlocks.get(0).getStartTime().getCal()
-        .get(Calendar.DAY_OF_MONTH);
-    mDateBlocks.add(new DateBlockData(new DateData(year, month, day, 0, 0, 0),
-        new DateData(year, month, day, 0, 0, 0)));
-    mDateBlocks.add(new DateBlockData(
-        new DateData(year, month, day, 23, 59, 59), new DateData(year, month,
-            day, 23, 59, 59)));
-    Collections.sort(mDateBlocks);
+  public void createFreeTimes(String[] filenames) {
+    ArrayList<DateBlockData> dateBlocks = fileListToDateBlocks(filenames);
+    if (dateBlocks.size() == 0) return;
     mSummary = "Free Time";
-    for (int i = 1; i < mDateBlocks.size(); i++) {
-      DateBlockData temp = DateBlockData.difference(mDateBlocks.get(i-1), 
-          mDateBlocks.get(i));
-      if (temp != null) {
-        mDateBlock = temp;
-        makeCalendar(Integer.toString(i) + ".ics");
+    addBeginEndTimes(dateBlocks);
+    Collections.sort(dateBlocks);
+    createFreeTimeFromDateBlocks(dateBlocks);
+  }
+  
+  public void createMeetingTimes(String[] list1, String[] list2) {
+    ArrayList<DateBlockData> dateBlocks1 = fileListToDateBlocks(list1);
+    ArrayList<DateBlockData> dateBlocks2 = fileListToDateBlocks(list2);
+    if (dateBlocks1.size() > 0 && dateBlocks2.size() > 0) {
+      // could make this more robust and check all dates
+      if (!DateData.isSameDate(dateBlocks1.get(0).getStartTime(), dateBlocks2.get(0).getStartTime())) {
+        System.out.println("Lists are not same Dates");
+        return;
       }
     }
+    ArrayList<DateBlockData> allDateBlocks = new ArrayList<DateBlockData>(dateBlocks1);
+    allDateBlocks.addAll(dateBlocks2);
+    if (allDateBlocks.size() == 0) return;
+    mSummary = "POSSIBLE MEETING TIME";
+    addBeginEndTimes(allDateBlocks); 
+    Collections.sort(allDateBlocks);
+    for (DateBlockData data : allDateBlocks) {
+      data.getStartTime().show();
+      data.getEndTime().show();
+    }
+    createFreeTimeFromDateBlocks(allDateBlocks);
   }
 
   public boolean grabPublic() {
@@ -269,6 +253,78 @@ public class CalendarMaker {
     writer.println("END:VCALENDAR");
     System.out.println("Finished Creating File: " + filename);
     writer.close();
+  }
+  
+  private void createFreeTimeFromDateBlocks(ArrayList<DateBlockData> dateBlocks) {
+    int fileIndex = 1;
+    int currentBlockIndex = 0;
+    for (int i = 1; i < dateBlocks.size(); i++) {
+      DateBlockData temp = DateBlockData.difference(
+          dateBlocks.get(currentBlockIndex), dateBlocks.get(i));
+      // if not overlapping blocks make a free time with the difference
+      if (temp != null) {
+        mDateBlock = temp;
+        currentBlockIndex = i;
+        makeCalendar(Integer.toString(fileIndex++) + ".ics");
+      } else {
+        // if the end of the second block is later than the first, we increase
+        // the first ones to the second
+        if (DateData.timeDifference(dateBlocks.get(currentBlockIndex)
+            .getEndTime(), dateBlocks.get(i).getEndTime()) > 0) {
+          dateBlocks.get(currentBlockIndex).setEndTime(
+              new DateData(dateBlocks.get(i).getEndTime()));
+        }
+      }
+    }   
+  }
+  
+  // makes array of DateBlocks that have the same date
+  private ArrayList<DateBlockData> fileListToDateBlocks(String[] filenames) {
+    ArrayList<DateBlockData> dateBlocks = new ArrayList<DateBlockData>();
+    for (String filename : filenames) {
+      if (validFile(filename)) {
+        File file = new File(filename);
+        Scanner fileScanner = null;
+        try {
+          fileScanner = new Scanner(file);
+        } catch (Exception e) {
+        }
+        String start = null;
+        String end = null;
+        // parse .ics for DTSTART and DTEND and init objects
+        while (fileScanner.hasNext()) {
+          String[] splitLine = fileScanner.nextLine().split(":");
+          if (stringMatch(splitLine[0], "DTSTART"))
+            start = splitLine[1];
+          if (stringMatch(splitLine[0], "DTEND"))
+            end = splitLine[1];
+        }
+        // if the .ics file has proper DTSTART and DTEND
+        if (start != null && end != null) {
+          DateBlockData block = new DateBlockData(start, end);
+          // all date blocks must be on the same day, only add if they are
+          if (dateBlocks.isEmpty()
+              || (!dateBlocks.isEmpty() && DateData.isSameDate(dateBlocks
+                  .get(0).getStartTime(), block.getStartTime()))) {
+            dateBlocks.add(block);
+          }
+        }
+      }
+    }   
+    return dateBlocks;
+  }
+  
+  private void addBeginEndTimes(ArrayList<DateBlockData> list) {
+    int year = list.get(0).getStartTime().getCal().get(Calendar.YEAR);
+    // add one because cal.get(Calendar.MONTH) starts at 0 for jan
+    int month = list.get(0).getStartTime().getCal().get(Calendar.MONTH) + 1;
+    int day = list.get(0).getStartTime().getCal()
+        .get(Calendar.DAY_OF_MONTH);
+    list.add(new DateBlockData(new DateData(year, month, day, 0, 0, 0),
+        new DateData(year, month, day, 0, 0, 0)));
+    list.add(new DateBlockData(
+        new DateData(year, month, day, 23, 59, 59), new DateData(year, month,
+            day, 23, 59, 59)));   
   }
 
   private boolean validFile(String filename) {
